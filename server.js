f3523b4c98227a549867aa3b5cb5e115892ddf83
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-03-09 21:59:52 +0530

    add routes
    basic route handling

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+app.use('/employees', require('./routes/api/employees'));
+
 app.get('/', (req, res) => {
   res.send("Hello World!");
 });
