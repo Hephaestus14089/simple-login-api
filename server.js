3d4c3628dd2805c92a6d1df611fb1a6992348519
Bhargav Das Gupta <dasguptabhargav@gmail.com>
2023-03-15 21:54:07 +0530

    minor bug fix

--- server.js
+++ server.js
@@ -13,4 +13,6 @@ app.get('/', (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}...`));
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}...`)
+});
